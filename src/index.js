bf0eabede94034c2a922b6c04d79ebac0b1c810d
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-01-09 16:00:22 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as Header from './header.js';
-import * as Main from './main.js';
+import * as Header from '/src/header.js';
+import * as Main from '/src/main.js';
 
 
 const body = document.querySelector(".body");
@@ -7,10 +7,47 @@ body.appendChild(Header.div);
 body.appendChild(Main.div);
 
 
-//npm install axios
-//npm install gsap
-//npm install node-sass
-//npm i @babel/core @babel/polyfill
-//npm install express
-//npm install @babel/core @babel/register @babel/preset-env --save-dev
-//npm install @bundled-es-modules/axios
+
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("Service Worker Work ")
+    }).catch(error => console.log("error : " + error))
+
+
+    // let deferredPrompt; // Allows to show the install prompt
+    // const installButton = document.getElementById("install_button");
+
+    // window.addEventListener("beforeinstallprompt", e => {
+    // console.log("beforeinstallprompt fired");
+    // // Prevent Chrome 76 and earlier from automatically showing a prompt
+    // e.preventDefault();
+    // // Stash the event so it can be triggered later.
+    // deferredPrompt = e;
+    // // Show the install button
+    // installButton.hidden = false;
+    // installButton.addEventListener("click", installApp);
+    // });
+
+    // window.addEventListener("appinstalled", evt => {
+    //     console.log("appinstalled fired", evt);
+    //   });
+}
+   
+
+// function installApp() {
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     installButton.disabled = true;
+  
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then(choiceResult => {
+//       if (choiceResult.outcome === "accepted") {
+//         console.log("PWA setup accepted");
+//         installButton.hidden = true;
+//       } else {
+//         console.log("PWA setup rejected");
+//       }
+//       installButton.disabled = false;
+//       deferredPrompt = null;
+//     });
+//   }
---
+++ sw.js
@@ -0,0 +1,55 @@
+const staticCacheName = "site-static-v1";
+const dynamicCache = "site-dynamic-v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/src/style.css",
+    "/src/index.js",
+    "/src/main.js",
+    "/src/header.js",
+    "/src/main/countriesList.js",
+    "/src/main/detail.js",
+    "/src/main/search.js",
+    "/images/icons/manifest-icon-512.png",
+    "/images/icons/manifest-icon-192.png",
+    "https://restcountries.eu/rest/v2/all",
+    "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
+];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key != staticCacheName)
+                .map(key => caches.delete(key)));
+        })
+    );
+});
+
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        // static fetch 
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request).then(fetchRes => {
+                // dynamic fetch 
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(e.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());

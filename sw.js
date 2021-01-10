1002a2af0e41faeea3a941b767dd5cf1bff0a1b3
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-01-10 16:36:37 +0100

    .

--- sw.js
+++ sw.js
@@ -16,6 +16,17 @@ const assets = [
     "/images/icons/manifest-icon-192.png",
     "/images/favicon-32x32.png",
     "/node_modules/gsap/all.js",
+    "/node_modules/gsap/gsap-core.js",
+    "/node_modules/gsap/CSSPlugin.js",
+    "/node_modules/gsap/Draggable.js",
+    "/node_modules/gsap/CSSRulePlugin.js",
+    "/node_modules/gsap/EaselPlugin.js",
+    "/node_modules/gsap/EasePack.js",
+    "/node_modules/gsap/MotionPathPlugin.js",
+    "/node_modules/gsap/PixiPlugin.js",
+    "/node_modules/gsap/ScrollToPlugin.js",
+    "/node_modules/gsap/ScrollTrigger.js",
+    "/node_modules/gsap/TextPlugin.js",
     "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
 ];
 // cache size limit;
